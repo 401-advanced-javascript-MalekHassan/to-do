e4c6b2def80c8dffdc692b26995c84f7d6738e73
MalekHassan <malekdalalha1995@gmail.com>
2020-11-25 09:14:47 +0200

    making the pagenation

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import SettingProvider from './context/setting.js';
 import ToDo from './components/todo/todo-connected';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <>
+export default function App() {
+  return (
+    <>
+      <SettingProvider>
         <ToDo />
-      </>
-    );
-  }
+      </SettingProvider>
+    </>
+  );
 }
--- src/components/todo/todo-connected.js
+++ src/components/todo/todo-connected.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import TodoForm from './formHooks';
 import TodoList from './list.js';
+import usePagination from '../../hooks/usePagintaion.js';
+import { SettingContext } from '../../context/setting.js';
+import Pages from '../pagination/pagination.js';
+import Setting from '../setting/setting-editor.js';
 
 import './todo.scss';
 import useAjax from '../hooks/useAjax';
@@ -9,6 +13,9 @@ const todoAPI = 'https://api-js401.herokuapp.com/api/v1/todo';
 
 const ToDo = () => {
   const [list, setList] = useState([]);
+  const [total, setTotal] = useStat([]);
+  const [page,setPage]= useState(1);
+  const {getItemsP} = usePagination{todoAPI , setList,list}
   const [getFunc, postFunc, putFunc, deletingFunc] = useAjax(list, setList);
 
   console.log('hi');
@@ -28,9 +35,29 @@ const ToDo = () => {
     deletingFunc(id);
     console.log('deleteeeeeeeee', id);
   };
+    const siteContext = useContext(SettingContext);
 
-  useEffect(_getTodoItems, []);
 
+  useEffect(()=>{
+    setPage(1)
+    _getTodoItems
+  }, [])
+
+  useEffect(() => {
+  _getTodoItems(siteContext.numberOfItems,page,total)
+  }, [page])
+   useEffect(() => {
+    getItemsP(siteContext.numberOfItems,page,total)
+   }, [total])
+   useEffect(() => {
+    if (siteContext.sorted === 'difficulty') {
+      let newTotal = total.sort((a, b) => {
+        return a.difficulty - b.difficulty;
+      });
+      setTotal(newTotal);
+    }
+    getItemsP(siteContext.numberOfItems, page, total);
+  }, [siteContext.sorted]);
   return (
     <>
       <header>
@@ -51,7 +78,9 @@ const ToDo = () => {
             handleComplete={putFunc}
             handleDelete={deletingTask}
           />
+          <Pages changePage={setPage} list={total}/>
         </div>
+        <Setting/>
       </section>
     </>
   );
